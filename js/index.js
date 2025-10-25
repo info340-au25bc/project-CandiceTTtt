087fb3f15c0be645ccd82d7176d1814315115043
CandiceTTtt <candice5tttt@gmail.com>
2025-10-25 01:46:38 -0700

    testing: add info for each emotion into the js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,6 @@ const moodMap = {
   exhausted:  { color: "#739DB7", icon: "icons/exhausted.png" }
 };
 
-
 const publicSongs = [
   {
     emotion: "happy",
@@ -35,6 +34,114 @@ const publicSongs = [
     icon: "icons/calm.png",
     likes: 31,
     saves: 8,
+    public: true
+  },
+  {
+    emotion: "relaxed",
+    color: "#C6EEDD",
+    song: "Soft Breeze",
+    artist: "Morning Whales",
+    link: "https://www.youtube.com/",
+    diary: "Took a long walk and let my mind wander 🌿",
+    icon: "icons/relaxed.png",
+    likes: 24,
+    saves: 5,
+    public: true
+  },
+  {
+    emotion: "angry",
+    color: "#FFB1C7",
+    song: "Crimson Flames",
+    artist: "Riot Echo",
+    link: "https://soundcloud.com/",
+    diary: "Traffic jam + loud horns… not the best day 😤",
+    icon: "icons/angry.png",
+    likes: 15,
+    saves: 2,
+    public: true
+  },
+  {
+    emotion: "lovely",
+    color: "#FFF1FF",
+    song: "Old Camera Roll",
+    artist: "Analog Hearts",
+    link: "https://www.youtube.com/",
+    diary: "Heard it at the café → memory unlocked ☕",
+    icon: "icons/lovely.png",
+    likes: 18,
+    saves: 5,
+    public: true
+  },
+  {
+    emotion: "excited",
+    color: "#EDD6DC",
+    song: "Starlight Drive",
+    artist: "The Sparkles",
+    link: "https://open.spotify.com/",
+    diary: "Concert night!!! Can't believe I was front row!! 🎤💫",
+    icon: "icons/excited.png",
+    likes: 56,
+    saves: 12,
+    public: true
+  },
+  {
+    emotion: "confused",
+    color: "#9ED3ED",
+    song: "Which Way Home",
+    artist: "Echo Glass",
+    link: "https://music.apple.com/",
+    diary: "That lyric hit too hard but I still don’t get it 🤔",
+    icon: "icons/confused.png",
+    likes: 19,
+    saves: 4,
+    public: true
+  },
+  {
+    emotion: "sad",
+    color: "#C7C9E2",
+    song: "Rainy Windows",
+    artist: "Mono Dream",
+    link: "https://soundcloud.com/",
+    diary: "Stayed in with tea and grey clouds ☁️",
+    icon: "icons/sad.png",
+    likes: 26,
+    saves: 6,
+    public: true
+  },
+  {
+    emotion: "tired",
+    color: "#739DB7",
+    song: "Fading Lights",
+    artist: "Slow Motion",
+    link: "https://open.spotify.com/",
+    diary: "Long day. I’ll just lie down with headphones on 😴",
+    icon: "icons/tired.png",
+    likes: 20,
+    saves: 5,
+    public: true
+  },
+  {
+    emotion: "exhausted",
+    color: "#B5E9EE",
+    song: "End of the Week",
+    artist: "Lo-Fi Room",
+    link: "https://music.apple.com/",
+    diary: "Finally done with everything... brain empty 🫠",
+    icon: "icons/exhausted.png",
+    likes: 13,
+    saves: 3,
+    public: true
+  },
+  {
+    emotion: "motivated",
+    color: "#9ED3ED",
+    song: "Keep Running",
+    artist: "Nova Drive",
+    link: "https://www.youtube.com/",
+    diary: "Morning jog playlist 🏃‍♀️✨ feeling unstoppable!",
+    icon: "icons/motivated.png",
+    likes: 39,
+    saves: 9,
     public: true
   }
 ];
